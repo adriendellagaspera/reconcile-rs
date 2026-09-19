ba8200548df9545a6657e70980fa72968acbb915
adriendellagaspera <32226398+adriendellagaspera@users.noreply.github.com>
2026-09-19 07:30:58 +0200

    Import construction error in tests

--- src/replicated_map/tests/construction.rs
+++ src/replicated_map/tests/construction.rs
@@ -13,7 +13,7 @@ use std::time::Duration;
 use tokio_util::sync::CancellationToken;
 
 use crate::clock::NodeId;
-use crate::{replicated_map::Config, ReplicatedMap};
+use crate::{replicated_map::{Config, ConstructionError}, ReplicatedMap};
 
 use super::ephemeral_config;
 
